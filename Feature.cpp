9f57bc57d6955689736f4e2b128f622e25ac4459
daviesw <daviesw@oregonstate.edu>
2017-07-23 17:21:37 -0700

    Update Feature.cpp

--- Feature.cpp
+++ Feature.cpp
@@ -6,6 +6,16 @@ Feature::Feature(std::string newName, std::vector<std::string> newDesc)
 	fDesc = newDesc;
 }
 
+Feature::Feature(std::string featName, std::vector<std::string> fDescs, std::vector<std::string> interactions, std::vector<bool> actions)
+{
+        fName = featName;
+        fDesc = fDescs;
+        interactionDesc = interactions;
+        nextAction = actions;
+        interactionNum = 0;
+}
+
+
 void Feature::setName(std::string name)
 {
 	fName = name;
