117e21e8126e4ab680f7d685d1ba33853bdbaa82
changyuj <changyuj@oregonstate.edu>
2017-07-23 20:01:19 -0500

    Update Room.cpp

--- Room.cpp
+++ Room.cpp
@@ -100,3 +100,87 @@ void Room::addItem(Item thing)
 	inRoom.erase(i);
 }*/
 
+//hardcoded comparison for now to show it works.
+bool Room::checkRoomNames(std::string rName)
+{
+    //compare to vector of rooms
+    //temp room name list
+
+    if(rName == "court")
+    {
+#ifdef NOISYTEST
+        std::cout << rName << " room found!" << std::endl;
+#endif
+        return true;
+    }
+    else
+        return false;
+}
+//hardcoded comparison for now to show it works until we have item working
+bool Room::checkItemInRoom(std::string rItem)
+{
+    if(rItem == "sword")
+    {
+#ifdef NOISYTEST
+        std::cout << rItem << " taken from room." << std::endl;
+#endif
+        return true;
+    }
+    else if(rItem == "chest")
+    {
+#ifdef NOISYTEST
+        std::cout << rItem << " is in the room." << std::endl;
+#endif
+        return true;
+    }
+    else if(rItem == "sphinx")
+    {
+#ifdef NOISYTEST
+        std::cout << rItem << " is in the room." << std::endl;
+#endif
+        return true;
+    }
+    else
+        return false;
+
+}
+
+void Room::setRooms(std::ifstream& readFile)
+{
+    std::string blank = "";
+    int exitDoors = 0; //read in number of exit doors in the room
+    std::string curDoor = "";
+    readFile >> blank >> roomNumber; //get the room number
+    readFile >> blank >> rName;
+    readFile >> blank;
+    std::getline(readFile, description); //read the description line
+    readFile >> blank >> exitDoors; //get the number of exit doors.
+    unsigned int i;
+    for(i = 0; i != exitDoors; i++)
+    {
+        readFile >> blank >> curDoor;
+        doors.push_back(curDoor); //get the door name
+    }
+}
+void Room::printRoomInfo()
+{
+	cout << "Room number: "<< roomNumber << endl;
+	cout << "Room name: " + rName << endl;
+	cout << "Room desccription: " <<  endl;
+	for(std::string::size_type i = 0; i != description.size(); i++)
+    {
+        cout << description[i];
+        if(description[i] == '.')
+        {
+            cout << endl;
+        }
+    }
+	cout << endl;
+	cout << "The door you see that you can to go to are " ;
+	for (Container::size_type i = 0; i != doors.size(); i++)//print doors
+	{
+		cout << " -->" << doors[i] << " --> ";
+	}
+	cout << endl;
+	cout << "What do you want to do ? " << endl;
+}
