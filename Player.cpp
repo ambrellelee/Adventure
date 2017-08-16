765aa94aac96d8b1ffa174d8b3524f01aafd386f
daviesw <daviesw@oregonstate.edu>
2017-08-15 23:27:09 -0700

    added new functions, updated constructors

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,13 @@
 #include "Player.hpp"
+#include "Item.hpp"
+#include "Inventory.hpp"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
 
+using namespace std;
 //Finish move; test drop and pickup methods
 
 //Constructors
@@ -12,6 +20,10 @@ Player::Player()
 	maxHitPoints = 20;
 	maxStamina = 10;
 	gameOver = false;
+        bag = Inventory("myInventory");
+        lastLocation = NULL;
+        currentLocation = NULL;
+
 }
 
 Player::Player(std::string newName, int hPoints, int pStamina, double pScore, Inventory pInvent)
@@ -21,6 +33,11 @@ Player::Player(std::string newName, int hPoints, int pStamina, double pScore, In
 	stamina = pStamina;
 	score = pScore;
 	bag = pInvent;
+	maxHitPoints = 20;
+	maxStamina = 10;
+	gameOver = false;
+	lastLocation = NULL;
+        currentLocation = NULL;
 }
 
 //Get and Set Methods
@@ -73,6 +90,34 @@ int Player::getScore()
 	return score;
 	std::cout << score << std::endl;
 }
+
+std::string Player::getCurLocName()
+{
+        return curLocationName;
+}
+
+std::string Player::getLastLocName()
+{
+        return lastLocationName;
+}
+
+void Player::setCurrentLocation(Room *curRoom)
+{
+        currentLocation = curRoom;
+        curLocationName = curRoom->getName();
+}
+
+void Player::setLastLocation(Room *lastRoom)
+{
+        lastLocation = lastRoom;
+        lastLocationName = lastRoom->getName();
+}
+
+Room *Player::getCurrentLocation()
+{
+        return currentLocation;
+}
+
 
 void Player::printPlayerInfo()
 {
@@ -217,3 +262,192 @@ void Player::loadPlayer(std::ifstream& loadPlayerFile)
 	//inFile >> info >> bag;
 }
 */
+int Player::savePlayer()
+{
+        fstream outputFile;
+        vector<Item> myStuff = bag.getStuff();
+        std::string fileName = "saveData/playerFile.txt";
+
+
+        outputFile.open(fileName.c_str(), ios::out);
+        if (!outputFile)
+        {
+                cout << "ERROR: Could not open file: " << fileName << endl;
+                cout << "Terminating savegame command" << endl;
+                return 1;
+        }
+
+        outputFile << pName << endl;
+        outputFile << hitPoints << endl;
+        outputFile << maxHitPoints << endl;
+        outputFile << stamina << endl;
+        outputFile << maxStamina << endl;
+        outputFile << score << endl;
+
+        if (currentLocation != NULL)
+        {
+                outputFile << currentLocation->getName() << endl;
+        }
+        else
+        {
+                outputFile << "NULL" << endl;
+        }
+        if (lastLocation != NULL)
+        {
+                outputFile << lastLocation->getName() << endl;
+        }
+        else
+        {
+                outputFile << "NULL" << endl;
+        }
+        outputFile << myStuff.size() << endl;
+        for (int i = 0; i < myStuff.size(); i++)
+        {
+                outputFile << myStuff[i].iName << endl;
+                outputFile << myStuff[i].iDesc << endl;
+                outputFile << myStuff[i].useDesc << endl;
+                outputFile << myStuff[i].waterLevel << endl;
+                outputFile << myStuff[i].maxWater << endl;
+
+                if (myStuff[i].available == true)
+                        outputFile << "True" << endl;
+                else
+                        outputFile << "False" << endl;
+
+                outputFile << myStuff[i].featureSource << endl;
+                outputFile << myStuff[i].whenCanTake << endl;
+
+        }
+
+        outputFile << bag.getName() << endl;
+
+        if (gameOver)
+                outputFile << "True" << endl;
+        else
+                outputFile << "False" << endl;
+
+        outputFile.close();
+        return 0;
+}
+
+int Player::loadPlayer()
+{
+        fstream inputFile;
+        vector<Item> myStuff;
+        bag.clearInventory();
+        std::string fileName = "saveData/playerFile.txt";
+        std::string line;
+        int numItems;
+        string iName, iDescription, iUse, waterLevel, maxWater, canGet;
+        string inFeature, interactionGet;
+        int waterNum, waterLimit, featNum, interactionNum;
+        bool canPickUp;
+
+        inputFile.open(fileName.c_str(), ios::in);
+        if (!inputFile)
+        {
+                cout << "ERROR: Could not open file: " << fileName << endl;
+                cout << "Terminating loadgame command" << endl;
+                return 1;
+        }
+
+        getline(inputFile, line);
+        pName = line;
+
+        getline(inputFile, line);
+        stringstream hitNum(line);
+        hitNum >> hitPoints;
+
+        getline(inputFile, line);
+        stringstream maxHit(line);
+        maxHit >> maxHitPoints;
+
+        getline(inputFile, line);
+        stringstream stamNum(line);
+        stamNum >> stamina;
+
+        getline(inputFile, line);
+        stringstream maxStam(line);
+        maxStam >> maxStamina;
+
+        getline(inputFile, line);
+        stringstream scoreNum(line);
+        scoreNum >> score;
+
+        getline(inputFile, line);
+
+        if (line != "NULL")
+                curLocationName = line;
+        else
+                curLocationName = "";
+
+        getline(inputFile, line);
+
+        if (line != "NULL")
+                lastLocationName = line;
+        else
+                lastLocationName = "";
+
+        getline(inputFile, line);
+        stringstream itemNum(line);
+        itemNum >> numItems;
+
+        for (int i = 0; i < numItems; i++)
+        {
+                getline(inputFile, line);
+                iName = line;
+
+                getline(inputFile, line);
+                iDescription = line;
+
+                getline(inputFile, line);
+                iUse = line;
+
+                getline(inputFile, line);
+                stringstream wLevel(line);
+                wLevel >> waterNum;
+
+                getline(inputFile, line);
+                stringstream limit(line);
+                limit >> waterLimit;
+
+                getline(inputFile, line);
+                if (line == "True")
+                        canPickUp = true;
+                else
+                        canPickUp = false;
+
+                getline(inputFile, line);
+                stringstream fNum(line);
+                fNum >> featNum;
+
+                getline(inputFile, line);
+                stringstream intNum(line);
+                intNum >> interactionNum;
+
+
+                Item newItem = Item(iName, iDescription, iUse, waterNum, waterLimit, canPickUp, featNum, interactionNum);
+                bag.addInventory(newItem);
+
+        }
+
+        getline(inputFile, line);
+        bag.setName(line);
+
+        getline(inputFile, line);
+        if (line == "True")
+                gameOver = true;
+        else
+                gameOver = false;
+
+        inputFile.close();
+
+
+        return 0;
+}
+
+void Player::printLocs()
+{
+        cout << currentLocation->getName() << endl;
+        cout << "lastLocation " << lastLocation->getName() << endl;
+}
